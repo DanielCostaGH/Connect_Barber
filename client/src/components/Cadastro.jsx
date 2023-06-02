603e3ee9bb926ec44b8a71c7f9b92a16a8dda993
DanielCostaGH <dfscs.costa@gmail.com>
2023-06-02 19:08:21 -0300

    FIX: Alterando o type do password.

--- client/src/components/Cadastro.jsx
+++ client/src/components/Cadastro.jsx
@@ -86,7 +86,7 @@ const Cadastro = () => {
               <div className="m-5 mx-auto">
                 <Field
                   className="placeholder-gray-900 placeholder-opacity-100 rounded-md h-inp w-auto sm:w-inp px-5"
-                  type="text"
+                  type="password"
                   id="password"
                   name="password"
                   placeholder="Senha   |"
